fcff1cb02ec8f9c8612f1bf25599b4f13f4cdb65
pushpa-info-14 <pushpa.info.14@gmail.com>
2025-01-14 16:08:36 +0530

    leetcode 219

--- leetcode/0-500/q219-contains-duplicate-ll.js
+++ leetcode/0-500/q219-contains-duplicate-ll.js
@@ -3,4 +3,25 @@
  * @param {number} k
  * @return {boolean}
  */
-var containsNearbyDuplicate = function (nums, k) {};
+var containsNearbyDuplicate = function (nums, k) {
+  const window = new Set();
+
+  let l = 0;
+  for (let r = 0; r < nums.length; r++) {
+    if (r - l > k) {
+      window.delete(nums[l]);
+      l += 1;
+    }
+    if (window.has(nums[r])) {
+      return true;
+    }
+    window.add(nums[r]);
+  }
+
+  return false;
+};
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3)); // true
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1)); // true
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)); // false
+console.log(containsNearbyDuplicate([1, 2, 3, 4, 5, 6, 7, 8, 9, 9], 3)); // true
